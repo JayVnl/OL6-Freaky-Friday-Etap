69a5a966d621fd21aa9e0035b7fae37e821b9d77
JayVnl <jorrit.v@outlook.com>
2022-04-01 16:56:11 +0200

    Topbar now has own component and finished basic layout of it

---
+++ Etap/Views/Components/TopBar.swift
@@ -0,0 +1,58 @@
+//
+//  TopBar.swift
+//  Etap
+//
+//  Created by Jorrit Verstappen on 01/04/2022.
+//
+
+import SwiftUI
+
+struct TopBar: View {
+	// MARK: PROPERTIES
+	
+	// MARK: BODY
+		var body: some View {
+			ZStack() {
+				GeometryReader { g in
+					VStack(alignment: .leading,spacing: 5) {
+						Rectangle()
+							.fill(Color("Primary"))
+							.frame(maxHeight: 120)
+						Rectangle()
+							.fill(Color("Primary"))
+							.frame(maxHeight: 5)
+						Rectangle()
+							.fill(Color("Primary"))
+							.frame(maxWidth: g.size.width * 0.75, maxHeight: 5)
+						Rectangle()
+							.fill(Color("Primary"))
+							.frame(maxWidth: g.size.width * 0.5, maxHeight: 5)
+					}
+				}.frame(maxHeight: 150)
+				
+				HStack {
+					VStack(alignment: .leading) {
+						Text("Etap")
+							.foregroundColor(Color.white)
+							.font(.system(size: 36))
+						Text("Every step counts")
+							.foregroundColor(Color.white)
+					} //: VSTACK
+					Spacer()
+					Text("1st of April")
+						.foregroundColor(Color.white)
+				} //: HSTACK
+				.frame(maxWidth: .infinity)
+				.padding(.horizontal, 20)
+				.padding(.bottom, 20)
+			} //: ZSTACK
+			.frame(maxWidth: .infinity)
+		}
+}
+
+// MARK: PREVIEW
+struct TopBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TopBar().previewLayout(.sizeThatFits)
+    }
+}
--- Etap/Views/HomeView.swift
+++ Etap/Views/HomeView.swift
@@ -12,9 +12,17 @@ struct HomeView: View {
 	
 	// MARK: BODY
     var body: some View {
-			VStack() {
+			VStack {
 				TopBar()
 				Spacer()
+				Rectangle()
+					.frame(maxWidth: 300, maxHeight: 300)
+				Spacer()
+				Rectangle()
+					.frame(maxWidth: 300, maxHeight: 90)
+				Spacer()
+				Rectangle()
+					.frame(maxWidth: 300, maxHeight: 80)
 			} //: VSTACK
 			.frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height).background(Color("Secondary")).edgesIgnoringSafeArea(.all)
     }
@@ -25,30 +33,4 @@ struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
     }
-}
-
-struct TopBar: View {
-		var body: some View {
-			ZStack {
-				Rectangle()
-					.fill(Color("Primary"))
-					.frame(maxHeight: 140)
-				
-				HStack {
-					VStack(alignment: .leading) {
-						Text("Etap")
-							.foregroundColor(Color.white)
-							.font(.system(size: 36))
-						Text("Every step counts")
-							.foregroundColor(Color.white)
-					} //: VSTACK
-					Spacer()
-					Text("1st of April")
-						.foregroundColor(Color.white)
-				} //: HSTACK
-				.frame(maxWidth: .infinity)
-				.padding(.horizontal, 20)
-			} //: ZSTACK
-			.frame(maxWidth: .infinity)
-		}
 }
