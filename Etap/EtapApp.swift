5e2bf9382f22812ed24bd3f642edaa34fdbd7c12
JayVnl <jorrit.v@outlook.com>
2022-04-01 15:33:00 +0200

    Initial Commit

---
+++ Etap/EtapApp.swift
@@ -0,0 +1,17 @@
+//
+//  EtapApp.swift
+//  Etap
+//
+//  Created by Jorrit Verstappen on 01/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct EtapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
